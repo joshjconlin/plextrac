8e462d2bb3aeb31936614c5e457e22eeebf60e14
Josh Conlin <joshjconlin@yahoo.com>
2019-09-27 13:29:12 -0600

    fix linting

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Typography from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper>
+      <Typography variant="h1">
+        Weather Page
+      </Typography>
+    </Paper>
   );
 }
 
